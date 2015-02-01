50cce659ac6bd194c21027a0f2d4e70ba6c22345
Oliver <Oliver@rpi-controlcenter.de>
2015-02-01 10:02:46 +0100

    Initial Commit

---
+++ switchserver/switchserver.ino
@@ -0,0 +1,22 @@
+/**
+ * Schaltserver für Arduino 
+ * 
+ * Benötigte Hardware:
+ * - Arduino Uno, Mega, Pro Mini oder Nano
+ * - 433MHz Sender
+ * - ENC28J60 Netzwerk Chip
+ *
+ * @author Oliver Kleditzsch
+ * @copyright Copyright (c) 2015, Oliver Kleditzsch
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+void setup() {
+	
+}
+
+void loop() {
+	
+}
